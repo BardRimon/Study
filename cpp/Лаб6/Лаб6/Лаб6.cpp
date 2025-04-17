7bdef56c137aa98c51e563d5260fca319a729810
Aleksandr <127941597+BardRimon@users.noreply.github.com>
2025-04-18 01:47:34 +0300

    Add files via upload

---
+++ cpp/Лаб6/Лаб6/Лаб6.cpp
@@ -0,0 +1,105 @@
+﻿// Лаб6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int iter;
+    int n, m, i, j, * A, * p, counter = 0, column = 0, c;
+    
+
+
+    cout << "Подсчёт через i (Введите 1) или через указатели (Введите 2)" << endl;
+    cin >> iter;
+    switch (iter)
+    {
+    case 1:
+
+        
+        cout << "Введите n и m:" << endl << "n = ";
+        cin >> n;
+        cout << "m = ";
+        cin >> m;
+        cout << endl;
+
+        A = new int[n * m];
+        srand((unsigned)time(0));
+        // наполнение матрицы из рандомными числами
+        for (p = A; p - A < n * m; p++)
+        {
+            *p = rand() % 10; // 10 было взято для того, чтобы вероятность получить 0 в столбце была выше
+        }
+        // вывод в консоль
+        cout << endl << "Ввод массива A[n][m]:" << endl << endl;
+        for (i = 0, p = A; i < n * m; i++, p++)
+        {
+            if (i % m == 0)
+            {
+                cout << endl;
+            }
+            cout.width(2);
+            cout << *p;
+        }
+        cout << endl;
+
+        for (i = 0; i < m; i++)
+        {
+            c = 0;
+            for (j = 0; j < n; j++, p++)
+            {
+                if (A[j * m + i] == 0)
+                {
+                    c++;
+                }
+
+            }
+            if (counter < c)
+            {
+                counter = c;
+                column = i;
+            }
+        }
+
+        cout << endl << "С наибольшим числом нулей = " << counter << " колонка №" << column + 1 << endl;
+
+        for (i = 0; i < n / 2; i++)
+        {
+            //cout << endl << A[i * m + column] << A[(n - 1 - i) * m + column];
+            j = A[i * m + column];
+            A[i * m + column] = A[(n - 1 - i) * m + column];
+            A[(n - 1 - i) * m + column] = j;
+
+            //cout << endl << A[i * m + column] << A[(n - 1 - i) * m + column];
+
+        }
+
+
+
+        cout << endl << "Обработанная матрица: " << endl;
+        for (i = 0, p = A; i < n * m; i++, p++)
+        {
+            if (i % m == 0)
+            {
+                cout << endl;
+            }
+            cout.width(2);
+            cout << *p;
+        }
+
+
+        delete[] A;
+
+    case 2:
+        cout << "-";
+    }
+    
+    
+
+}
+
---
+++ cpp/Лаб6_2/Лаб6_2/Лаб6_2.cpp
@@ -0,0 +1,112 @@
+﻿// Лаб6_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	int n, m, c, max = 0, column = 0;
+	cout << "Input N: ";
+	cin >> n;
+	cout << "\nInput M: ";
+	cin >> m;
+	cout << endl;
+
+	srand((unsigned)time(0));
+	//создаём массив из n указателей по m
+	int** A = new int* [n];
+	int** prow = A;
+	while (prow - A < n)
+	{
+		*prow = new int[m];
+		int* pi = *prow;
+		int* pend = pi + m;
+		while (pi < pend) {
+			*pi = rand() % 10;
+			pi++;
+		}
+		prow++;
+	}
+
+	// выводим массив в консоль
+	prow = A;
+	while (prow - A < n)
+	{
+		int* pi = *prow;
+		int* pend = pi + m;
+		while (pi < pend)
+		{
+			cout << *pi << " ";
+			pi++;
+		}
+		cout << endl;
+		prow++;
+	}
+
+
+	prow = A;
+	int* pi = *prow;
+	int i = 0;
+	while (i < m)
+	{
+		c = 0;
+		for (prow = A; prow - A < n; prow++)
+		{
+			//int* pi = *prow;
+			pi = *prow;
+
+			pi += i;
+			if (*pi == 0)
+			{
+				c++;
+			}
+		}
+		if (c > max)
+		{
+			max = c;
+			column = i;
+		}
+		i++;
+	}
+
+
+	cout << endl << "В колонке " << column + 1;
+	cout << " Наибольшее число 0 равное " << max << endl;
+
+	int a, b, index;
+	for (prow = A; prow - A < n / 2; prow++)
+	{	
+		index = prow - A;
+		int* pi1 = A[index] + column; 
+		int* pi2 = A[n - 1 - index] + column;
+		int temp = *pi1;
+		*pi1 = *pi2;
+		*pi2 = temp;
+
+
+		
+
+	}
+
+	cout << "массив с инверсированным столбцом" << endl;
+
+	prow = A;
+	while (prow - A < n)
+	{
+		int* pi = *prow;
+		int* pend = pi + m;
+		while (pi < pend)
+		{
+			cout << *pi << " ";
+			pi++;
+		}
+		cout << endl;
+		prow++;
+	}
+
+}
+
